8ca88ebe3174bf4e8f68c79af4cf2cd0d2972686
Chris Liu <chris@ixvin.com>
2025-09-08 14:43:43 +0800

    改进sql结果的判断，区分是否为查询结果。

--- server/src/db/adapters/mysql.ts
+++ server/src/db/adapters/mysql.ts
@@ -1,17 +1,29 @@
 import type { ISqlResult } from "@server/db";
 
+import type { OkPacket } from "mysql";
 
 export class MysqlAdapter implements IDatabaseAdatpter {
     public normalize<T = any>(input: any): ISqlResult<T> {
         //todo: 区分是查询还是其他
 
         log("MysqlAdapter", input);
-        if (Array.isArray(input)) {
-            const rows = input[0] ?? input;
-            return {
-                rows,
-                affectedRows: Array.isArray(rows) ? rows.length : 0,
-            };
+        if (Array.isArray(input) && input.length > 0) {
+            // 不是查询类 sql
+            if (_isOkPacket(input[0])) {
+                return {
+                    rows: [],
+                    affectedRows: input[0].affectedRows,
+                };
+            }
+
+            const rows = input[0];
+            // 是查询结果集
+            if (Array.isArray(rows)) {
+                return {
+                    rows,
+                    affectedRows: -1,
+                };
+            }
         }
 
         return {
@@ -20,4 +32,11 @@ export class MysqlAdapter implements IDatabaseAdatpter {
         };
     }
 
+}
+
+function _isOkPacket(p: any): p is OkPacket {
+    return typeof p.fieldCount === "number" &&
+        typeof p.affectedRows === "number" &&
+        typeof p.message === "string" &&
+        typeof p.changedRows === "number";
 }
